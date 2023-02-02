c4a9dd7112cbf132bfc12f9b76807b6419e4547b
webdevodd <103882490+ronetinc@users.noreply.github.com>
2023-02-02 18:09:38 +0600

    version 2.0 - upload#1:020223 editos#3
    tutloadbar

---
+++ scripts/js/tutloadbar.js
@@ -0,0 +1,23 @@
+var links = document.getElementsByTagName("a"),
+i = 0, l = links.length,
+body = document.body;
+
+for(;i<l;i++) {
+    links[i].addEventListener("click",function(){
+        body.className = "page-loading";
+    },false);
+}
+for(;i<l;i++) {
+    links[i].addEventListener("click",function(){
+        body.className = "page-loading";
+        setTimeout(function(){
+            body.className = "";
+        },3000);
+    },false);
+}
+window.addEventListener("beforeunload",function(e){
+    document.body.className = "page-loading";
+},false);
+window.addEventListener("beforeunload",function(e){
+    document.body.className = "page-loading";
+},false);
